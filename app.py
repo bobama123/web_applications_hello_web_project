4be46de22cc88e9f71f0c511e240579b850983c9
Bobby Chaudhry <bobbychaudhry@hotmail.co.uk>
2024-05-02 21:11:34 +0100

    complete

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    name = request.form['name']
+    message = request.form['message']
+
+    return f'Thanks {name}, you sent this message: "{message}"'
+
+@app.route('/wave', methods=['GET'])
+def get_wave():
+    name = request.args['name']
+
+    return f"I am waving at {name}"
+
 # == Example Code Below ==
 
 # GET /emoji
@@ -15,6 +28,31 @@ app = Flask(__name__)
 @app.route('/emoji', methods=['GET'])
 def get_emoji():
     return ":)"
+
+@app.route('/count_vowels', methods=['POST'])
+def post_count_vowels():
+    text = request.form['text']
+    vowel_number = 0
+    for character in text:
+        if character in 'aeiou':
+            vowel_number += 1
+    
+    return f'There are {vowel_number} vowels in "{text}"'
+
+@app.route('/sort_names', methods=['POST'])
+def post_sort_names():
+    names = request.form['names']
+    names_list = names.split(",")
+    sort_list = sorted(names_list)
+    string = ",".join(sort_list)
+    return string
+
+@app.route('/add', methods=['GET'])
+def post_add_name():
+    name = request.args['name']
+    names = 'Julia, Alice, Karim'
+    return f"{names}, {name}"
+
 
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
@@ -28,4 +66,6 @@ apply_example_routes(app)
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
+
 
